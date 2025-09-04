3807c9fd6b63169e3de7bcde8756375cde938e3d
Santi Panyanoi <aon007567@gmail.com>
2025-09-04 17:16:09 +0700

    Update Homework6 6-5.c

--- Lap/Homework6 6-5.c
+++ Lap/Homework6 6-5.c
@@ -1,1 +1,80 @@
+//แบบใช้ for loop
+#include <stdio.h>
 
+int main() {
+    int n;
+    printf("Enter number : ");
+    scanf("%d", &n);
+
+    for (int i = n; i >= 2; i--) {       // นับจาก n ลงมาถึง 2
+        int isPrime = 1;                 // สมมติว่าเป็นจำนวนเฉพาะ
+        for (int j = 2; j * j <= i; j++) {
+            if (i % j == 0) {
+                isPrime = 0;
+                break;
+            }
+        }
+        if (isPrime)
+            printf("%d ", i);
+    }
+
+    return 0;
+}
+
+//แบบใช้ while loop
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter number : ");
+    scanf("%d", &n);
+
+    int i = n;
+    while (i >= 2) {
+        int isPrime = 1;
+        int j = 2;
+        while (j * j <= i) {
+            if (i % j == 0) {
+                isPrime = 0;
+                break;
+            }
+            j++;
+        }
+        if (isPrime)
+            printf("%d ", i);
+        i--;
+    }
+
+    return 0;
+}
+
+//แบบใช้ do...while loop
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter number : ");
+    scanf("%d", &n);
+
+    int i = n;
+    if (i >= 2) {
+        do {
+            int isPrime = 1;
+            int j = 2;
+            do {
+                if (j * j > i) break;
+                if (i % j == 0) {
+                    isPrime = 0;
+                    break;
+                }
+                j++;
+            } while (j * j <= i);
+
+            if (isPrime)
+                printf("%d ", i);
+            i--;
+        } while (i >= 2);
+    }
+
+    return 0;
+}
